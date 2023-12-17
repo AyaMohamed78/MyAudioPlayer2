4e8025191b7431c89cd6b067d0087cbaaf2c36dc
Aya Mohamed <aya.ames@gmail.com>
2023-12-17 18:32:11 +0200

    first

---
+++ app/src/main/java/com/example/myaudioplayer/MainActivity.java
@@ -0,0 +1,154 @@
+package com.example.myaudioplayer;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1;
+     static ArrayList<MusicFiles> musicFiles ;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        permission();
+
+    }
+
+    private void permission() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_CODE);
+            System.out.println("The text here");
+        }
+        else {
+            System.out.println("The second one ");
+
+            musicFiles= getAllAudio(this );
+            initViewPager();
+        }
+            }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, do your initialization or tasks here
+
+                musicFiles = getAllAudio(this );
+                initViewPager();
+            } else {
+                // Permission denied, show a message or handle accordingly
+                Toast.makeText(this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+                // You might want to close the app or disable functionality that requires this permission
+            }
+        }
+    }
+
+    private void initViewPager() {
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        TabLayout tabLayout =findViewById(R.id.tab_layout);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        // Add fragments with titles
+        viewPagerAdapter.addFragment(new SongFragment(), "Songs");
+        viewPagerAdapter.addFragment(new AlbumFragment(), "Albums");
+
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private static class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> fragmentList = new ArrayList<>();
+        private final List<String> fragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager supportFragmentManager) {
+            super(supportFragmentManager);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            fragmentList.add(fragment);
+            fragmentTitleList.add(title);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitleList.get(position);
+        }
+    }
+    public static ArrayList<MusicFiles> getAllAudio(Context context){
+        ArrayList<MusicFiles> tempAudioList = new ArrayList<>();
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String[] projection ={
+                MediaStore.Audio.Media.ALBUM,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DURATION,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.ARTIST
+        };
+        Cursor cursor = context.getContentResolver().query(
+                uri,
+                projection,
+                null, // assuming no selection criteria
+                null, // assuming no selection arguments
+                null  // assuming no sorting
+        );
+        if (cursor !=null){
+            while (cursor.moveToNext()) {
+                String album = cursor.getString(0); // column index for album
+                String title = cursor.getString(1); // column index for title
+                String duration = cursor.getString(2); // column index for duration
+                String path = cursor.getString(3); // column index for path
+                String artist = cursor.getString(4); // column index for artist
+
+                MusicFiles musicFiles = new MusicFiles(path, title, artist, album, duration);
+                Log.e("Path", path);
+                Log.e("Album", album);
+                tempAudioList.add(musicFiles);
+
+            }
+            cursor.close();
+
+        }
+        return tempAudioList;
+
+    }
+}
