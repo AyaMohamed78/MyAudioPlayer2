21f951ca7285a53b72ce939f1015d27ca867baef
Aya Mohamed <aya.ames@gmail.com>
2023-12-18 08:28:49 +0200

    second

--- app/src/main/java/com/example/myaudioplayer/Player.java
+++ app/src/main/java/com/example/myaudioplayer/Player.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Player extends AppCompatActivity {
     TextView song_name, artist_name, duration_played, duration_total;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
